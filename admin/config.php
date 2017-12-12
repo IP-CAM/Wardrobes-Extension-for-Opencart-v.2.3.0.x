fda2c8ebc8084dac0bae3b65221ab0a2d87abea9
IvanSavelev <ivan.sav.86@mail.ru>
2017-12-12 06:56:27 +0300

    First commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart-2.3-wardrobes/admin/');
+define('HTTP_CATALOG', 'http://opencart-2.3-wardrobes/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart-2.3-wardrobes/admin/');
+define('HTTPS_CATALOG', 'http://opencart-2.3-wardrobes/');
+
+// DIR
+define('DIR_APPLICATION', 'W:/domains/opencart-2.3-wardrobes/admin/');
+define('DIR_SYSTEM', 'W:/domains/opencart-2.3-wardrobes/system/');
+define('DIR_IMAGE', 'W:/domains/opencart-2.3-wardrobes/image/');
+define('DIR_LANGUAGE', 'W:/domains/opencart-2.3-wardrobes/admin/language/');
+define('DIR_TEMPLATE', 'W:/domains/opencart-2.3-wardrobes/admin/view/template/');
+define('DIR_CONFIG', 'W:/domains/opencart-2.3-wardrobes/system/config/');
+define('DIR_CACHE', 'W:/domains/opencart-2.3-wardrobes/system/storage/cache/');
+define('DIR_DOWNLOAD', 'W:/domains/opencart-2.3-wardrobes/system/storage/download/');
+define('DIR_LOGS', 'W:/domains/opencart-2.3-wardrobes/system/storage/logs/');
+define('DIR_MODIFICATION', 'W:/domains/opencart-2.3-wardrobes/system/storage/modification/');
+define('DIR_UPLOAD', 'W:/domains/opencart-2.3-wardrobes/system/storage/upload/');
+define('DIR_CATALOG', 'W:/domains/opencart-2.3-wardrobes/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'mysql');
+define('DB_PASSWORD', 'mysql');
+define('DB_DATABASE', 'opencart-2.3-wardrobes');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
